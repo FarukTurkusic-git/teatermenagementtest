628c18992ecf53408e8f59765929bfe6e9752b17
Faruk Turkusic <faruk.turkusic@onbe.com>
2022-01-31 12:59:14 +0100

    change add arguments

--- theatermenagementtest/Tests/SmokeTests/SmokeTest.cs
+++ theatermenagementtest/Tests/SmokeTests/SmokeTest.cs
@@ -16,15 +16,18 @@ namespace AcceptanceTest.Tests.SmokeTests
         public void BrowseToUrlPageTest()
         {
             ChromeOptions options = new ChromeOptions();
-            options.AddArguments("test-type");
-            options.AddArguments("start-maximized");
-            options.AddArguments("--window-size=1920,1080");
-            options.AddArguments("--enable-precise-memory-info");
-            options.AddArguments("--disable-popup-blocking");
-            options.AddArguments("--disable-default-apps");
-            options.AddArguments("test-type=browser");
+            //options.AddArguments("test-type");
+            //options.AddArguments("start-maximized");
+            //options.AddArguments("--window-size=1920,1080");
+            //options.AddArguments("--enable-precise-memory-info");
+            //options.AddArguments("--disable-popup-blocking");
+            //options.AddArguments("--disable-default-apps");
+            //options.AddArguments("test-type=browser");
             options.AddArguments("--disable - dev - shm - usage");
+            options.AddArguments("--remote-debugging-port=9222");
             
+
+
 
             ChromeDriver Driver = new ChromeDriver(options);
 
